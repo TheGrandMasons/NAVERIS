a671423ced89dee554f891b9d9c322c43100cbef
Seif Zakaria Mohamed Ellaban <30405200107815@sci.asu.edu.eg>
2023-09-22 21:37:44 +0300

    def plates

---
+++ Backend/plates.py
@@ -0,0 +1,20 @@
+plates = (
+    "African",
+    "Antarctic",
+    "Arabian",
+    "Australian",
+    "Caribbean",
+    "Cocos",
+    "Eurasian",
+    "Indian",
+    "Juan de Fuca",
+    "Nazca",
+    "North American",
+    "Pacific",
+    "Philippine Sea",
+    "Scotia",
+    "South American"
+)
+plate = input("Enter a plate: ")
+if plate in plates:
+    print("True")
