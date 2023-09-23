89b32a1659beedc11d7939dc20f1742d5e06ba51
Omar Ellaban <113574311+TheBruteforcer@users.noreply.github.com>
2023-09-23 05:02:37 +0300

    Stuffed Edges

--- cityJson.py
+++ cityJson.py
@@ -1,31 +1,22 @@
-# Importing libraries
+# Importing Libraries.
 import requests
 import json
-
+# func
+def Status(lon, lat)
+# Requests Constants.
 API_Key = "77df8ee93034bedcbe6b96b0f9eb9f0a"
-
-city_name = input("Enter city name: ")
-
-url = f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_Key}"
-
+url = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid={API_Key}"
+# Sending Req.
 response = requests.get(url)
-
+# Get The Response With Json Format
 res = response.json()
 print(res)
 
 if res["cod"] != "404":
     data = res['main']
-
-    live_temperature = data["temp"]
-
-    live_pressure = data["pressure"]
-    desc = res["weather"]
-
-    weather_description = desc[0]["description"]
-    print("Temperature (in Kelvin scale): " + str(live_temperature))
-    print("Pressure: " + str(live_pressure))
-    print("Description: " + str(weather_description))
-
+    live_wind_speed = data["current"]["wind_speed"]
+    live_pressure = data["current"]["pressure"]
+    live_wind_degree = data["current"]["wind_deg"]
+    return (live_wind_speed, live_wind_degree, live_pressure)
 else:
-
-    print("Please enter a valid city name")
+    return "Error While Fetching The Response Of API Request."
