b86c28031b0d6ec29d909d1be9994f7e9a06ed19
Seif Zakaria Mohamed Ellaban <30405200107815@sci.asu.edu.eg>
2023-10-05 18:03:25 +0300

    update

--- Backend/naveris.py
+++ Backend/naveris.py
@@ -10,4 +10,5 @@ from plotter import *
 # Lat & Long are for geopositioning.
 # Range is how many (Lat or Long) lines will be passed.
 # Resolution is the distance between each dot measured in (Lat or Long).
-draw(10, 10, 2, 0.1)
+# draw(46.2102, -39.3989, 10, 0.5)
+# drawJSON()
