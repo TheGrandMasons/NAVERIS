0bec026fffdd117f77d71b53b236c0978710bb03
Omar Ellaban <omar.ellaban00@gmail.com>
2023-10-05 13:27:41 +0200

    storm detection system

--- Backend/stormDetect.py
+++ Backend/stormDetect.py
@@ -1,6 +1,10 @@
 import json
-def detect(database):
-    database2 = open(database)
-    file = json.load(database2)
-    print(file[1])
+def detect(raw_database):
+    database = open(raw_database)
+    file = json.load(database)
+    for single_raw in file:
+        ws = single_raw["ws"]
+        ap = single_raw["ap"]
+        if ws > 49:
+            
 detect("dotMatrix.json")
