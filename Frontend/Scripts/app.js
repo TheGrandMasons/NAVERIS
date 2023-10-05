5eec648080d1307c4bdeabb6c36187371647f721
a04k <63ahmedkhaled@gmail.com>
2023-10-05 17:40:34 +0300

    ya rab fok el deka

--- Frontend/Scripts/app.js
+++ Frontend/Scripts/app.js
@@ -1,18 +1,10 @@
 var mainsection = document.getElementById("main")
-var infobtn = document.getElementById("infoBtn")
-var infosection = document.getElementById("infoSection")
 var mapbtn = document.getElementById("mapBtn")
 var mapsection = document.getElementById("mapSection")
 
-
-infobtn.addEventListener('click', function() {
-  console.log("confirmito")
-  infosection.style.display = 'flex';
-  infosection.scrollIntoView({ behavior: 'smooth' }); 
   
-});
 mapbtn.addEventListener('click', function() {
-  console.log("confirmito2")
+  console.log("confirmito2");
   mapsection.style.display = 'flex';
   mapsection.scrollIntoView({ behavior: 'smooth' }); 
   
