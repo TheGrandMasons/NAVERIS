723faccee09bb5d44f8ffd702d659688145ab672
Omar Ellaban <omar.ellaban00@gmail.com>
2023-10-06 03:39:26 +0200

    updated alert system brrr

--- AlertSYS.py
+++ AlertSYS.py
@@ -1,5 +1,6 @@
 import json
 from random import randint
+import requests
 s = []
 count = 0
 with open("dotMatrix.json", "r") as db:
@@ -8,27 +9,43 @@ with open("dotMatrix.json", "r") as db:
         count += 1 
         if float(db2["ws"]) >= 65 and int(db2["ap"]) <= 1010 or float(db2["ws"]) >= 65:
             prb = randint(99,80)
+            lat = db2["lat"]
+            lon = db2["lon"]
+            limit = 2
+            key = "77df8ee93034bedcbe6b96b0f9eb9f0a"
+            res = requests.post(f'http://api.openweathermap.org/geo/1.0/reverse?lat={lat}&lon={lon}&limit={limit}&appid={key}')
+
             s.append({
                 "id" : count,
                 "wm" : "D",
                 "pb" : f"{prb}%",
-                "cc" : "NULL"
+                "cc" : res.json()["country"]
             })
         if float(db2["ws"]) >= 50 and int(db2["ap"]) <= 1010 or float(db2["ws"]) >= 50:
             prb = randint(30,60)
+            lat = db2["lat"]
+            lon = db2["lon"]
+            limit = 2
+            key = "77df8ee93034bedcbe6b96b0f9eb9f0a"
+            res = requests.post(f'http://api.openweathermap.org/geo/1.0/reverse?lat={lat}&lon={lon}&limit={limit}&appid={key}')
             s.append({
                 "id" : count,
                 "wm" : "W",
                 "pb" : f"{prb}%",
-                "cc" : "NULL"
+                "cc" : res.json()['country']
             })
         if float(db2["ws"]) >= 40 and int(db2["ap"]) <= 1010 or float(db2["ws"]) >= 40:
             prb = randint(0,30)
+            lat = db2["lat"]
+            lon = db2["lon"]
+            limit = 2
+            key = "77df8ee93034bedcbe6b96b0f9eb9f0a"
+            res = requests.post(f'http://api.openweathermap.org/geo/1.0/reverse?lat={lat}&lon={lon}&limit={limit}&appid={key}')
             s.append({
                 "id" : count,
                 "wm" : "N",
                 "pb" : f"{prb}%",
-                "cc" : "NULL"
+                "cc" : res.json()['country']
             })
         file = open('alert.json', 'w')
         json.dump(s,file,indent=4)
